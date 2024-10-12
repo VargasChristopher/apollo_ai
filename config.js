714e2c82c15fccd5ca822254fcb1e65552d94447
Christopher Vargas <37318105+VargasChristopher@users.noreply.github.com>
2024-10-12 18:46:18 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
-const OPENAI_API_KEY = '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';
+const OPENAI_API_KEY = 'Insert API Key Here';
 
 module.exports = { OPENAI_API_KEY };
